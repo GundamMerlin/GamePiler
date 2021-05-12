7c7c92ce701a747d0c9dec13426425356236e366
Francis N <gundammerlin@gmail.com>
2021-05-12 16:54:51 -0500

    deleted Redirect

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect, useHistory } from 'react-router';
+import { useHistory } from 'react-router';
 import { signInUser } from './../services';
 import { useState } from 'react'
 
