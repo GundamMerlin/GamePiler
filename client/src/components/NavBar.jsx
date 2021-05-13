c59038361d946dbbdb9cf08bb9311f9dc7863b92
Francis N <gundammerlin@gmail.com>
2021-05-13 17:09:24 -0500

    Easter Egg Fun

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,7 +12,9 @@ export default function NavBar(props) {
   };
 
   return (
-    <div className = "NavBar">
+    <div className="NavBar">
+      <div id="hidden">"blastoise"
+        <img id="blastoise" src="https://raw.githubusercontent.com/GundamMerlin/GamePiler/main/assets/clipart1977071.png"></img></div>
       <Link to ="/backlogs"><button>Home</button></Link>
       <Link to="/add-game"><button>Add Game</button></Link>
       <Link to="/add-console"><button>Add Console</button></Link>
